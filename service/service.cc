1f3c17e4051c9fa4455980071a5ecab552e916f8
Duncan Smith <dsmith1974@gmail.com>
2014-01-11 10:44:21 +0000

    use exe name for logfile

--- service/service.cc
+++ service/service.cc
@@ -4,23 +4,25 @@
 
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
 #include <boost/log/attributes/attribute.hpp>
 #include <boost/log/attributes/attribute_cast.hpp>
 #include <boost/log/attributes/attribute_value.hpp>
-#include <boost/log/attributes/current_thread_id.hpp>
 #include <boost/log/attributes/current_process_name.hpp>
-#include <boost/log/trivial.hpp>
+#include <boost/log/attributes/current_thread_id.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/sources/logger.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/severity_feature.hpp>
 #include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_feature.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
 #include <boost/move/utility.hpp>
 
 #include <service/args.h> // NOLINT
@@ -28,6 +30,7 @@
 namespace src = boost::log::sources;
 namespace attrs = boost::log::attributes;
 namespace expr = boost::log::expressions;
+namespace fs = boost::filesystem;
 
 namespace osoa {
 
@@ -51,11 +54,13 @@ void Service::Initialize(int argc, const char *argv[]) {
   
   boost::log::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity"); 
 
+  fs::path full_path(argv[0]);
+  
   add_file_log (
-    keywords::file_name = "file_%Y-%m-%d_%H-%M-%S.%N.log",                                        
+    keywords::file_name = full_path.filename().string() + "_%Y-%m-%d_%H-%M-%S.%N.log",                                        
     keywords::rotation_size = 10 * 1024 * 1024,                                   
     keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0), 
-    keywords::format = "[%TimeStamp%] [%Process%] [%ThreadID%] [%Severity%]: %Message%",
+    keywords::format = "[%TimeStamp%] [%Process%] [%Severity%] [%ThreadID%]: %Message%",
     keywords::filter = expr::attr<severity_level>("Severity") >= trivial::debug
   );
 
@@ -65,7 +70,6 @@ void Service::Initialize(int argc, const char *argv[]) {
 
   core::get()->add_global_attribute("ThreadID", attrs::current_thread_id());
   core::get()->add_global_attribute("Process", attrs::current_process_name());
-  //core::get()->add_global_attribute("Severity", trivial::severity);
 
   auto& lg = svc_logger::get();
   BOOST_LOG_SEV(lg, trivial::info) << "Started the service.";
