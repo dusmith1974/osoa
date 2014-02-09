7e03c6bb73cd5bf59261d98c576320d6e9d07253
Duncan Smith <dsmith1974@gmail.com>
2014-02-09 09:38:09 +0000

    resolve listening port from etc/services, populate service map

--- service/comms.cc
+++ service/comms.cc
@@ -6,7 +6,6 @@
 
 #include "boost/algorithm/string.hpp"
 #include "boost/array.hpp"
-#include "boost/asio.hpp"
 // #include "boost/bind.hpp"
 // #include "boost/date_time/posix_time/posix_time.hpp"
 
@@ -26,22 +25,26 @@ void print_count(const boost::system::error_code&,
   }
 }*/
 
-namespace asio = boost::asio;
-using boost::asio::ip::tcp;
 
 namespace osoa {
-
-// http://www.iana.org/assignments/service-names-port-numbers/
-//   service-names-port-numbers.xhtml?&page=125
-const int base_port = 35007;  // to 35353 unassigned (2014-02-02)
-const int listening_port = base_port + 0;
 
 // Iterative Server (handle one connection at a time).
 int Comms::Publish() {
   try {
     asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(tcp::v4(), "127.0.0.1", "osoa");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    
+    // http://www.iana.org/assignments/service-names-port-numbers/
+    //   service-names-port-numbers.xhtml?&page=125
+    int port = 0;
+    for (; endpoint_iterator != tcp::resolver::iterator(); ++ endpoint_iterator)
+      port = endpoint_iterator->endpoint().port();
+
     tcp::acceptor acceptor(io_service,
-                           tcp::endpoint(tcp::v4(), listening_port));
+                           tcp::endpoint(tcp::v4(), port));
 
     int connections = 0;
     for (;;) {
@@ -70,14 +73,18 @@ int Comms::Subscribe(const std::vector<std::string>& subscriptions) {
     if (server_service.size() < 2)
       continue;
 
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(server_service[0], server_service[1]);
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-    tcp::socket socket(io_service);
-    asio::connect(socket, endpoint_iterator);
-
     try {
+      tcp::resolver resolver(io_service);
+      tcp::resolver::query query(server_service[0], server_service[1]);
+      tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+      service_map()[server_service[0]] = std::make_pair(
+        std::make_shared<tcp::socket>(io_service), 
+        std::make_shared<tcp::resolver::iterator>(resolver.resolve(query)));
+
+      tcp::socket socket(io_service);
+      asio::connect(socket, endpoint_iterator);
+
       for (;;) {
         boost::array<char, 128> buf;
         boost::system::error_code error;
--- service/comms.h
+++ service/comms.h
@@ -3,20 +3,38 @@
 #ifndef SERVICE_COMMS_H_
 #define SERVICE_COMMS_H_
 
+#include <map>
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
+#include "boost/asio.hpp"
 #include "boost/noncopyable.hpp"
 
 namespace osoa {
 
+namespace asio = boost::asio;
+using boost::asio::ip::tcp;
+
+typedef std::pair<std::shared_ptr<tcp::socket>, 
+                  std::shared_ptr<tcp::resolver::iterator>> SocketPointPair;
+
+typedef std::map<std::string, SocketPointPair> ServiceMap;
+
 class Comms : boost::noncopyable {
  public:
-  Comms() {}
+  Comms() : service_map_{} {}
   ~Comms() {}
 
   int Publish();
   int Subscribe(const std::vector<std::string>& subscriptions);
+ 
+  const ServiceMap& service_map() const { return service_map_; }
+
+ private:
+  ServiceMap service_map_;
+  ServiceMap& service_map() { return service_map_; }
 };
 
 }  // namespace osoa
