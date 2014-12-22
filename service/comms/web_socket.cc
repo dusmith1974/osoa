c213b6ba932f05c4a5551c561edb1d71cdef12db
Duncan Smith <duncan.smith@telperin.com>
2014-12-22 20:39:45 +0000

    WebSocket.

--- service/comms/web_socket.cc
+++ service/comms/web_socket.cc
@@ -18,7 +18,10 @@
 #include "osoa_pch.h"
 #include "service/comms/web_socket.h"
 
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 #include "boost/asio.hpp"
 
@@ -48,10 +51,10 @@ using Poco::ThreadPool;
 using Poco::Util::ServerApplication;
 using Poco::Util::Application;
 
-//std::unique_ptr<Poco::Net::WebSocket> ws_;
-//int flags;
-
 namespace osoa {
+
+std::mutex m;
+std::condition_variable cv;
 
 void handler(const boost::system::error_code& error, int /*signal_number*/) {
   if (!error) {
@@ -69,36 +72,46 @@ class WebSocketRequestHandler : public HTTPRequestHandler {
     try {
       int opcodes = 0;
       Poco::Net::WebSocket ws(request, response);
+
+      // TODO(ds) to log
       std::cout << std::string("WebSocket connection established.") << std::endl;
+
       char buffer[1024];
       int n;
       int flags = 0;
       int beat_num = 0;
       size_t msg_num = 1;
+      std::cv_status status = std::cv_status::no_timeout;
       do {
         n = ws.receiveFrame(buffer, sizeof(buffer), flags);
         opcodes = flags & Poco::Net::WebSocket::FRAME_OP_BITMASK;
 
         std::cout << Poco::format("Frame received (length=%d, flags=0x%x, msg=%d).", n, unsigned(flags), ++beat_num) << std::endl; 
 
-        flags &= ~Poco::Net::WebSocket::FRAME_OP_BITMASK;
-
-        if (messages_.size() >= msg_num) {
+        // Send any new messages to the web client..
+        if (status == std::cv_status::no_timeout) {
+          flags &= ~Poco::Net::WebSocket::FRAME_OP_BITMASK;
           flags |= Poco::Net::WebSocket::FRAME_OP_TEXT;
-          ws.sendFrame(messages_[msg_num].data(), messages_[msg_num].size(), flags);
-          msg_num++;
+          while (messages_.size() >= msg_num) {
+            ws.sendFrame(messages_[msg_num].data(), messages_[msg_num].size(), flags);
+            msg_num++;
+          }
         }
 
+        // .. followed by a PING to keep things moving.
         flags &= ~Poco::Net::WebSocket::FRAME_OP_BITMASK;
         flags |= Poco::Net::WebSocket::FRAME_OP_PING;
-        ws.sendFrame(buffer, 5, flags);
+        ws.sendFrame(buffer, 0, flags);
 
-        // TODO(DS) Replace sleep with a condition variable.
-        if (messages_.size() < msg_num)
-          boost::this_thread::sleep(boost::posix_time::milliseconds(250));
+        // Wait for a new message to arrive or send another PING after 1s.
+        {
+          std::unique_lock<std::mutex> lk(m);
+          status = cv.wait_for(lk, std::chrono::seconds(1));
+        }
       } while (n > 0 || opcodes != Poco::Net::WebSocket::FRAME_OP_CLOSE);
 
-      //std::cout << "WebSocket connection closed." << std::endl;
+      // TODO(DS) to log
+      std::cout << "WebSocket connection closed." << std::endl;
     }
     catch (WebSocketException& exc) {
       std::cout << exc.what() << std::endl;
@@ -116,10 +129,6 @@ class WebSocketRequestHandler : public HTTPRequestHandler {
       }
     }
   }
-
-  /*void SendMessage(const std::string& message) {
-    ws_->sendFrame(message.data(), message.length(), flags);
-  }*/
 
  public:
    //std::unique_ptr<Poco::Net::WebSocket> ws_;
@@ -177,10 +186,6 @@ void WebSocket::Run() {
     // start the HTTPServer
     srv.start();
 
-    //std::string message("hooray!");
-    //ws_->sendFrame(message.data(), message.length(), flags);
-
-
     // wait for CTRL-C or kill
     boost::asio::io_service io_service;
     boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
@@ -193,6 +198,7 @@ void WebSocket::Run() {
 
 void WebSocket::PublishMessage(const std::string& msg) {
   messages_[messages_.size() + 1] = msg;
+  cv.notify_all();
 }
 
 }  // namespace osoa
